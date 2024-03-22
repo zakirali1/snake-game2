650c7e7354f16bf9d0f9b7719668c8811b0308dc
Zak <zakirali1@hotmail.co.uk>
2024-03-22 14:31:10 +0000

    initial commit

---
+++ snake-game/src/main/java/zak/snake/Main.java
@@ -0,0 +1,23 @@
+package zak.snake;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+        int tileSize = 25;
+
+        JFrame frame = new JFrame("Snake");
+
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+    }
+}
